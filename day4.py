2812083160b31ab1c71b6595c51fe8f7bf287ab5
jfbfacistol <jfb.facistol@gmail.com>
2026-03-25 23:53:48 +0800

    Day 4 COMPLETE: Engineered single-purpose cleaning functions and structured dictionary payloads

---
+++ day4.py
@@ -0,0 +1,26 @@
+# --- THE RAW DATA ---
+building_name = "Eastwood Global Plaza"
+raw_price = "  PHP 150,000,000.00 "
+raw_size = "2500 sqm"
+
+def clean_price(raw_price):
+    # Strip all leading and trailing whitespace. 
+    # Remove the lowercase letters PHP
+    clean_number = raw_price.strip().replace("PHP", "").replace(",","")
+    return float(clean_number)
+
+def clean_size(raw_size):
+    # Strip all leading and trailing whitespace. 
+    # Remove the lowercase letters "sqm"
+    clean_area = raw_size.strip().replace("sqm", "")
+    return float(clean_area)
+
+
+property_profile = {
+    "name" : building_name,
+    "price" : clean_price(raw_price),
+    "gla" : clean_size(raw_size)
+}
+
+print(f"--- System Payload ---\n {property_profile}")
+print(f"Target Price Extraction: {property_profile['price']}")
